3c76c76913a90e34bb6ab45282c80e78e96bb3f3
Angad <anilterapalli@gmail.com>
2022-12-17 11:30:27 +0530

    First Commit

---
+++ src/main/java/com/mycompany/gitdemo/GitDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitdemo;
+
+/**
+ *
+ * @author 91809
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/gitdemo/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gitdemo;
+
+/**
+ *
+ * @author 91809
+ */
+public class main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
